e83d9dd7255f1e5bd769757f29183a4f196eae36
unknown <michalislaqqas@gmail.com>
2019-11-04 19:24:59 +0200

    AAAAA

--- sketch.js
+++ sketch.js
@@ -1,28 +1,26 @@
 let g = -0.5;
 var balls = [];
+let x = 10;
+
+function preload(){
+  ballSprite = loadImage("assets/yeet.png");
+
+}
 
 function setup() {
   createCanvas(1500,800);
+  imageMode(CENTER);
 }
 function draw() {
   background(0,0,0);
-  rectMode(CENTER)
+  rectMode(CENTER);
+
 
   //make mouse on cursor
   rect(mouseX,mouseY,20,20)
 
   //for every ball placed
-  for (let i = 0; i < balls.length; i++){
-    //apply gravity to ball
-    moveBall(balls[i]);
-    //draw ball
-    ellipse(balls[i].relativeX,balls[i].relativeY, 20, 20);
-
-    //if ball fall out of screen hihi
-    if(balls[i].relativeY > height){
-      balls.splice(i,1);
-    }
-  }
+  doBall();
 
   //debug shit
   if(balls.length > 0)
@@ -33,36 +31,36 @@ function draw() {
 }
 
 function mousePressed(){
+    placeBall();
+}
 
-
+function placeBall(){
   //make ball object
   ballObject = new Ball(mouseX,mouseY,0,random(-0.1,0.1));
-
   //put ball object in ball array
   append(balls,ballObject);
 }
 
-
-function moveBall(ball){
-  //apply gravity
-  if (ball.t>0){
-  ball.setRelY(ball.y + (1/2 * ball.g * ball.t * ball.t));
-  //move ball horizontally
-  ball.setRelX(ball.x + ball.u * ball.t);
-}else if(ball.t == -500){
-  ball.setRelY(ball.y);
-  ball.setRelX(ball.x);
+function doBall(){
+  for (let i = 0; i < balls.length; i++){
+    //apply gravity to ball
+    balls[i].moveBall();
+    //draw ball
+    //image(ballSprite,balls[i].relativeX,balls[i].relativeY, 50, 50);
+    ellipse(balls[i].relativeX,balls[i].relativeY, 50, 50);
+    //if ball fall out of screen hihi
+    if(balls[i].relativeY > height || balls[i].relativeY < 0){
+      balls.splice(i,1);
+    }
+  }
 }
-  ball.setTime(ball.t + 1);
-}
-
 
 class Ball {
-  g = 0;
-  x = 0;
-  y = 0;
-  u = 0;
-  t = -500;
+  g;
+  x;
+  y;
+  u;
+  t = 0;
 
   relativeY = 0;
   relativeX = 0;
@@ -74,15 +72,13 @@ class Ball {
     this.g = g;
   }
 
-  setRelY(n){
-    this.relativeY = n;
-  }
-
-  setTime(n){
-    this.t = n;
-  }
-
-  setRelX(n){
-    this.relativeX = n;
+  moveBall(){
+    //apply gravity
+    if (this.t>=0){
+      this.relativeY = this.y + (1/2 * this.g * this.t * this.t);
+      //move ball horizontally
+      this.relativeX = this.x + this.u * this.t;
+    }
+    this.t += 1;
   }
 }
